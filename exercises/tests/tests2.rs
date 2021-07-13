3b1efa8f03bcb2279da42f40bdf7f9679001b701
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-07-13 14:57:21 +0200

    Tests

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,14 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let mut x: Vec<i32> = Vec::new();
+        x.push(1);
+        x.push(2);
+        x.push(3);
+        assert_eq!(x, vec![1, 2, 3]);
     }
 }
